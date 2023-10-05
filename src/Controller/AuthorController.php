54f2d0be1036a1f8897d2e65b5085c2c69234b84
Ben khalifa Ghada <benkhalifaghada@gmail.com>
2023-10-05 13:55:23 +0100

    doctrine_part1

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AuthorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthorController extends AbstractController
+{
+    #[Route('/author', name: 'app_author')]
+    public function index(): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+    #[Route('/list',name:'list')]
+    public function list(AuthorRepository $repo){
+       $list=$repo->findAll();
+        return $this->render('author/list.html.twig',['Authors'=>$list]);
+    }
+}
